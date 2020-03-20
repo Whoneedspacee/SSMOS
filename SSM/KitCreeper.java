79b2fa3dcd39996d8a73f2ad3b0a0c09748f0099
Space <yougotreallyowned@gmail.com>
2020-03-19 21:55:58 -0400

    ok its good

---
+++ SSM/KitCreeper.java
@@ -0,0 +1,35 @@
+package me.SirInHueman.SSM;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class KitCreeper extends me.SirInHueman.SSM.Kit
+{
+    
+	public KitCreeper()
+	{
+		super();
+		
+		this.name = "Creeper";
+
+		this.armor = {
+			Material.IRON_BOOTS,
+			Material.LEATHER_LEGGINGS,
+			Material.LEATHER_CHESTPLATE,
+			Material.LEATHER_HELMET
+		};
+		
+		this.weapons = {
+			Material.IRON_AXE,
+			Material.IRON_SHOVEL
+		};
+		
+		this.abilities = {
+			new SulphurBomb(),
+			new Explode()
+		};
+	}
+	
+}
