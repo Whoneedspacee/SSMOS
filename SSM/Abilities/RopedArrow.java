811fe381402614d9c5164aa725de0dee3270831a
Space <yougotreallyowned@gmail.com>
2020-03-20 18:00:32 -0400

    abilities and kits can now listen for events
    roped arrow is fixed
    
    fixes jump pads
    
    health regen and hunger loss is now cancelled

--- SSM/Abilities/RopedArrow.java
+++ SSM/Abilities/RopedArrow.java
@@ -4,11 +4,16 @@ import SSM.*;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 public class RopedArrow extends Ability {
+
+    Arrow arrow;
+    Player player;
 
     public RopedArrow() {
         this.name = "Roped Arrow";
@@ -17,13 +22,18 @@ public class RopedArrow extends Ability {
     }
 
     public void useAbility(Player player) {
-        Arrow arrow = player.launchProjectile(Arrow.class);
+        this.player = player;
+        arrow = player.launchProjectile(Arrow.class);
         arrow.setCustomName("Roped Arrow");
         arrow.setDamage(6.0);
         arrow.setVelocity(player.getLocation().getDirection().multiply(1.8D));
     }
 
-    public void pullToArrow(Player player, Arrow arrow) {
+    @EventHandler
+    public void pullToArrow(ProjectileHitEvent e) {
+        if(e.getEntity() != arrow) {
+            return;
+        }
         Vector p = player.getLocation().toVector();
         Vector a = arrow.getLocation().toVector();
         Vector pre = a.subtract(p);
