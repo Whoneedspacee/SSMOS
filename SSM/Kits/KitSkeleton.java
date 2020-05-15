1e4c05edf22c929afdf39412ce676f4a2c158c94
Whoneedspacee <yougotreallyowned@gmail.com>
2020-05-15 01:45:19 -0400

    Update KitSkeleton.java

--- SSM/Kits/KitSkeleton.java
+++ SSM/Kits/KitSkeleton.java
@@ -20,11 +20,11 @@ public class KitSkeleton extends Kit {
     public KitSkeleton() {
         super();
 
-        this.damage = 5;
-        this.armor = 6;
-        this.speed = 0.21f;
-        this.regeneration = 0.2;
-        this.knockback = 1.25;
+        this.damage = 4;
+        this.armor = 7;
+        this.speed = 0.24f;
+        this.regeneration = 0.25;
+        this.knockback = 1.9;
         this.disguise = DisguiseType.SKELETON;
         this.name = "Skeleton";
         this.menuItem = Material.BOW;
@@ -33,15 +33,15 @@ public class KitSkeleton extends Kit {
     public void equipKit(Player player) {
         super.equipKit(player);
 
-        setArmor(Material.CHAINMAIL_BOOTS, 0);
-        setArmor(Material.CHAINMAIL_LEGGINGS, 1);
-        setArmor(Material.CHAINMAIL_CHESTPLATE, 2);
-        setArmor(Material.CHAINMAIL_HELMET, 3);
+        setArmor(Material.CHAINMAIL_BOOTS, 3);
+        setArmor(Material.CHAINMAIL_LEGGINGS, 2);
+        setArmor(Material.CHAINMAIL_CHESTPLATE, 1);
+        setArmor(Material.CHAINMAIL_HELMET, 0);
 
         setItem(Material.IRON_AXE, 0, new BoneExplosion());
         setItem(Material.BOW, 1, new RopedArrow());
 
-        addAttribute(new ItemGenerator(Material.ARROW, 1, 3, 3));
+        addAttribute(new ItemGenerator(Material.ARROW, 1, 3, 5));
         addAttribute(new Regeneration(regeneration, 1));
         addAttribute(new GenericDoubleJump(0.61, 0.8, 1, Sound.ENTITY_GHAST_SHOOT));
         addAttribute(new Barrage(1.1, 0.25, 5));
