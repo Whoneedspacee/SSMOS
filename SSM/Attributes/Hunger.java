dc0432a2f4aa31007df4912907457f8d54059c38
huxsowo <62276741+huxsowo@users.noreply.github.com>
2020-05-25 15:45:09 -0400

    Fixed a bunch of glitches and started working on hunger

--- SSM/Attributes/Hunger.java
+++ SSM/Attributes/Hunger.java
@@ -1,4 +1,20 @@
 package SSM.Attributes;
 
-public class Hunger {
+import SSM.Attribute;
+
+public class Hunger extends Attribute {
+
+    double hungerLoss;
+    double delay;
+
+    public Hunger(){
+        super();
+        this.name = "Hunger";
+        task = this.runTaskTimer(plugin, 0, (long) delay*20);
+    }
+
+    @Override
+    public void run(){
+        owner.setFoodLevel(owner.getFoodLevel()-1);
+    }
 }
--- SSM/Kits/KitSquid.java
+++ SSM/Kits/KitSquid.java
@@ -1,22 +1,14 @@
 package SSM.Kits;
 
 import SSM.Abilities.InkShotgun;
-import SSM.Abilities.SpinWeb;
-import SSM.Abilities.SuperSquid;
 import SSM.Attributes.DoubleJumps.GenericDoubleJump;
 import SSM.Attributes.Regeneration;
 import SSM.Attributes.SquidFix;
 import SSM.Kit;
 import me.libraryaddict.disguise.disguisetypes.DisguiseType;
-import me.libraryaddict.disguise.disguisetypes.MobDisguise;
-import me.libraryaddict.disguise.disguisetypes.watchers.SlimeWatcher;
-import me.libraryaddict.disguise.disguisetypes.watchers.SquidWatcher;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
-import SSM.Ability;
 
 public class KitSquid extends Kit {
 
@@ -41,7 +33,7 @@ public class KitSquid extends Kit {
         setArmor(Material.CHAINMAIL_CHESTPLATE, 2);
 
         setItem(Material.IRON_AXE, 0, new InkShotgun());
-        setItem(Material.IRON_SWORD, 1, new SuperSquid());
+        setItem(Material.IRON_SWORD, 1);
         setItem(Material.IRON_SHOVEL, 2);
 
         addAttribute(new Regeneration(regeneration, 1));
