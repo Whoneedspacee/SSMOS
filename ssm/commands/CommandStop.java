a8639bc499cf861931a5e5358cc537e076fa94e2
whoneedspacee <yougotreallyowned@gmail.com>
2023-11-21 14:02:19 -0500

    Edits stop command so it works in the hub

--- ssm/commands/CommandStop.java
+++ ssm/commands/CommandStop.java
@@ -13,16 +13,17 @@ public class CommandStop implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String commandLabel, String[] args) {
-        if (!(commandSender instanceof Player)) {
-            Bukkit.shutdown();
+        if(!commandSender.isOp()) {
             return true;
         }
-        if(!commandSender.isOp()) {
+        if (!(commandSender instanceof Player)) {
+            Bukkit.shutdown();
             return true;
         }
         Player player = (Player) commandSender;
         SmashServer server = GameManager.getPlayerServer(player);
         if(server == null) {
+            Bukkit.shutdown();
             return true;
         }
         server.stopGame();
